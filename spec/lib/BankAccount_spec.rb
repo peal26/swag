af925fda3f528f8cc2a70d3b6d49a1056c1db7f9
Phanhiran <peal_26@hotmail.com>
2014-12-01 14:22:43 +0700

    update2

--- spec/lib/BankAccount_spec.rb
+++ spec/lib/BankAccount_spec.rb
@@ -6,12 +6,12 @@ describe BankAccount do
     it "check the deposit" do
    	  account1=BankAccount.new(5000)
       balance=account1.deposit(40000)
-      expect(balance).to eq(4500)
+      expect(balance).to eq(45000)
     end
     it "check the withdraw" do
    	  account1=BankAccount.new(5000)
       balance=account1.withdraw(500)
-      expect(balance).to eq(4500)
+      expect(balance).to eq(450)
     end
     it "check the transaction" do
    	  account1=BankAccount.new(5000)
